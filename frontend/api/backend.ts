12bac1b1e24cd558512a1a1ef858463c5fc33739
onggi <onggisanjaya@gmail.com>
2025-02-20 09:56:08 +0700

    perbaikan vercel

--- frontend/api/backend.ts
+++ frontend/api/backend.ts
@@ -1,5 +1,5 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  res.json({ message: "Halo dari backend yang berjalan di Vercel!" });
+  res.json({ message: "Halo dari backend di Vercel dengan Express!" });
 }

4e447bc3d5979a5e2a67d01f235f9a6d5cf7eaee
onggi <onggisanjaya@gmail.com>
2025-02-24 14:02:12 +0700

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/", // Sesuaikan jika deploy ke subfolder
-  server: {
-    port: 3000,
-    open: true, // Buka browser otomatis
-  },
+  // server: {
+  //   port: 3000,
+  //   open: true, // Buka browser otomatis
+  // },
   build: {
     outDir: "dist", // Pastikan hasil build ada di dist/
   }
